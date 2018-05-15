7fe2109cc74cb7a5fdd23a2b2153cc5ae4eb4537
Chepsi <kipkoech13@gmail.com>
2018-05-15 19:50:45 +0300

    Finished the Login and Signup trials with Firebase

---
+++ AndroidStudioProjects/Msafiri/app/src/main/java/com/dev/chepsi/msafiri/LoginActivity.java
@@ -0,0 +1,61 @@
+package com.dev.chepsi.msafiri;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText loginEmail, loginPass;
+    private Button loginBtn;
+    private ProgressBar loginProgress;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+
+        loginEmail = findViewById(R.id.signInEmail);
+        loginPass = findViewById(R.id.signInPassword);
+        loginBtn = findViewById(R.id.sign_in_button);
+        loginProgress = findViewById(R.id.signinProgress);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                String email = loginEmail.getText().toString().trim();
+                final String password = loginPass.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(getApplicationContext(), "Input email", Toast.LENGTH_SHORT).show();
+                }
+                if (TextUtils.isEmpty(password)){
+                    Toast.makeText(getApplicationContext(), "Input password", Toast.LENGTH_SHORT).show();
+                }
+
+                loginProgress.setVisibility(View.VISIBLE);
+
+                auth.signInWithEmailAndPassword(email,password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                loginProgress.setVisibility(View.GONE);
+                            }
+                        });
+            }
+        });
+    }
+}
