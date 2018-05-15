d89410fa035240f5df0bc7216522571e0758c86d
Chepsi <kipkoech13@gmail.com>
2018-05-15 22:37:11 +0300

    Finished the Login and Signup trials with Firebase

--- AndroidStudioProjects/Msafiri/app/src/main/java/com/dev/chepsi/msafiri/LoginActivity.java
+++ AndroidStudioProjects/Msafiri/app/src/main/java/com/dev/chepsi/msafiri/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.dev.chepsi.msafiri;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
