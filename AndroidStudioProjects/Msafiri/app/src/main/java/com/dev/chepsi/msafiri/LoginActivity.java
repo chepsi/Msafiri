0b5a32b4e3eea30ed0cf902227cfc932ee9f7435
Chepsi <kipkoech13@gmail.com>
2018-06-20 12:01:35 +0300

    Bug fixes.

--- AndroidStudioProjects/Msafiri/app/src/main/java/com/dev/chepsi/msafiri/LoginActivity.java
+++ AndroidStudioProjects/Msafiri/app/src/main/java/com/dev/chepsi/msafiri/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.dev.chepsi.msafiri;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -21,6 +23,7 @@ public class LoginActivity extends AppCompatActivity {
     private Button loginBtn;
     private ProgressBar loginProgress;
     private FirebaseAuth auth;
+    private TextView signUpLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +36,18 @@ public class LoginActivity extends AppCompatActivity {
         loginPass = findViewById(R.id.signInPassword);
         loginBtn = findViewById(R.id.sign_in_button);
         loginProgress = findViewById(R.id.signinProgress);
+        signUpLink = findViewById(R.id.signUpLink);
+
+        loginProgress.setVisibility(View.GONE);
+
+        signUpLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,SignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,13 +64,23 @@ public class LoginActivity extends AppCompatActivity {
 
                 loginProgress.setVisibility(View.VISIBLE);
 
-                auth.signInWithEmailAndPassword(email,password)
-                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                loginProgress.setVisibility(View.GONE);
-                            }
-                        });
+                try {
+
+                    auth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    loginProgress.setVisibility(View.GONE);
+                                    Intent openMain = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(openMain);
+                                    finish();
+                                }
+                            });
+
+                }
+                catch (Exception e){
+                    Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
